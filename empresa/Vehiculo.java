2c6e7da2a6f585dab75fe7e7f11dbbbc9c5d683b
lunanocuac-coder <lunanocuac@gmail.com>
2026-04-16 12:20:40 -0500

    Create Vehiculo.java

---
+++ empresa/Vehiculo.java
@@ -0,0 +1,19 @@
+class Vehiculo {
+    protected String placa;
+    protected String marca;
+    protected double velocidadMaxima;
+
+    public Vehiculo(String placa, String marca, double velocidadMaxima) {
+        this.placa = placa;
+        this.marca = marca;
+        this.velocidadMaxima = velocidadMaxima;
+    }
+
+    public void mover() {
+        System.out.println("El vehículo se está moviendo");
+    }
+
+    public double calcularPeaje() {
+        return 0;
+    }
+}
